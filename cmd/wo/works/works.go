b72783307645129038f144beb76111d3bd2475c6
ali-furkan <a.furkan.2003@gmail.com>
2021-09-12 20:09:26 +0300

    :sparkles:: add move cmd at root

--- cmd/wo/works/works.go
+++ cmd/wo/works/works.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ali-furkan/wo/cmd/wo/works/cmd_init"
 	"github.com/ali-furkan/wo/cmd/wo/works/create"
 	"github.com/ali-furkan/wo/cmd/wo/works/list"
+	"github.com/ali-furkan/wo/cmd/wo/works/move"
 	"github.com/ali-furkan/wo/cmd/wo/works/open"
 	"github.com/ali-furkan/wo/cmd/wo/works/remove"
 	"github.com/ali-furkan/wo/cmd/wo/works/run"
@@ -19,6 +20,7 @@ func InitCmdWorks(cmd *cobra.Command, cfg *config.Config) *cobra.Command {
 	cmd.AddCommand(open.NewCmdOpen(cfg))
 	cmd.AddCommand(run.NewCmdRun(cfg))
 	cmd.AddCommand(list.NewCmdList(cfg))
+	cmd.AddCommand(move.NewCmdMove(cfg))
 
 	return cmd
 }
