91bf577afffbe855798f55aa8d2e439f6c35dbe3
ali-furkan <a.furkan.2003@gmail.com>
2021-09-28 16:12:57 +0300

    :fire:: rm uncreated auth struct

--- internal/config/config_file.go
+++ internal/config/config_file.go
@@ -1,13 +1,11 @@
 package config
 
 import (
-	"github.com/ali-furkan/wo/internal/auth"
 	"github.com/ali-furkan/wo/internal/editor"
 	"github.com/ali-furkan/wo/internal/workspace"
 )
 
 type ConfigFile struct {
-	Auth      map[string]auth.Auth `yaml:"auth"`
 	Editors   []editor.Editor      `yaml:"editors"`
 	Workspace workspace.Workspace  `yaml:"workspace"`
 }
--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -1,14 +1,12 @@
 package config
 
 import (
-	"github.com/ali-furkan/wo/internal/auth"
 	"github.com/ali-furkan/wo/internal/editor"
 	"github.com/ali-furkan/wo/internal/workspace"
 )
 
 var defaultConfigFile = &ConfigFile{
 	Editors: []editor.Editor{},
-	Auth:    make(map[string]auth.Auth),
 	Workspace: workspace.Workspace{
 		DefaultEditor:  "",
 		DefaultReadme:  true,
