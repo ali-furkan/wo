cfbb3c09f7d5d4c57648cc56b967d9c3a254c4ae
Ali Furkan <46776661+ali-furkan@users.noreply.github.com>
2021-11-22 01:29:20 +0300

    :white_check_mark:: add new version test

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -1,33 +1,97 @@
 package version
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
 
-type args struct {
-	av string
-	bv string
-}
-
 type testCase struct {
 	name string
-	args args
-	want bool
+	args interface{}
+	want interface{}
 }
 
 func TestCreateNewVersion(t *testing.T) {
+	const (
+		major = 10
+		minor = 11
+		patch = 12
+		tag   = "alpha"
+
+		errNotMatchFieldsFormat = "version %s doesn't match the %s: want '%d' -> got '%d'"
+	)
+
+	verFormat := fmt.Sprintf("v%d.%d.%d-%s", major, minor, patch, tag)
+
+	ver, err := NewVersion(verFormat)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ver.Major != major {
+		t.Fatalf(errNotMatchFieldsFormat, "major", "major", major, ver.Major)
+	}
+
+	if ver.Minor != minor {
+		t.Fatalf(errNotMatchFieldsFormat, "minor", "minor", minor, ver.Minor)
+	}
+
+	if ver.Patch != patch {
+		t.Fatalf(errNotMatchFieldsFormat, "patch", "patch", patch, ver.Patch)
+	}
+
+	if ver.VersionName != tag {
+		t.Fatalf("version tag doesn't match the tag: want '%s' -> got '%s'", tag, ver.VersionName)
+	}
+
+	if ver.String() != verFormat {
+		t.Fatalf("ver convert string func works wrongly: got %s -> want %s", ver.String(), verFormat)
+	}
 }
 
-// func TestBenchmarkCreateVersion() {
+func benchmarkCreateVersion(ver string, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := NewVersion(ver)
+		if err != nil {
+			b.Fail()
+		}
+	}
+}
 
-// }
+func BenchmarkCreateVersionWithTag(b *testing.B) {
+	benchmarkCreateVersion("v31.15.7-alpha", b)
+}
 
-// func TestBenchmarkFormatString() {
+func BenchmarkCreateVersionWithoutTag(b *testing.B) {
+	benchmarkCreateVersion("v31.15.7", b)
+}
 
-// }
+func benchmarkFormatString(ver string, b *testing.B) {
+	v, err := NewVersion(ver)
+	if err != nil {
+		b.Fail()
+	}
+
+	for n := 0; n < b.N; n++ {
+		v.String()
+	}
+}
+
+func BenchmarkFormatStringWithTag(b *testing.B) {
+	benchmarkFormatString("v31.15.7-alpha", b)
+}
+
+func BenchmarkFormatStringWithoutTag(b *testing.B) {
+	benchmarkFormatString("v31.15.7", b)
+}
 
 func TestIsGreaterThan(t *testing.T) {
+	type args struct {
+		av string
+		bv string
+	}
+
 	testCases := []testCase{
 		{"It should return false when it was got equal versions (with tags)", args{"v1.0.0-beta", "v1.0.0"}, false},
 		{"It should return false when it was got equal versions (without tags) ", args{"v1.0.0", "v1.0.0"}, false},
@@ -42,20 +106,49 @@ func TestIsGreaterThan(t *testing.T) {
 	for _, tC := range testCases {
 		t.Run(tC.name, func(t *testing.T) {
 			// CompareVersion does the same job as v.IsGreaterThan
-			got := CompareVersion(tC.args.av, tC.args.bv)
+			got := CompareVersion(tC.args.(args).av, tC.args.(args).bv)
 
-			if got != tC.want {
-				t.Errorf("v.IsGreaterThan() = %v, want %v", got, tC.want)
+			if got != tC.want.(bool) {
+				t.Errorf("v.IsGreaterThan() = %v, want %v", got, tC.want.(bool))
 			}
 		})
 	}
 }
 
-// func TestBenchmarkIsGreaterThan() {
+func BenchmarkIsGreaterThan(b *testing.B) {
+	type args struct {
+		av string
+		bv string
+	}
 
-// }
+	testCases := []testCase{
+		{"It should return false when it was got equal versions (with tags)", args{"v1.0.0-beta", "v1.0.0"}, false},
+		{"It should return false when it was got equal versions (without tags) ", args{"v1.0.0", "v1.0.0"}, false},
+		{"A should be greater than B version in major", args{"v2.0.0", "v1.0.0"}, true},
+		{"A should be greater than B version in minor", args{"v1.1.0", "v1.0.0"}, true},
+		{"A should be greater than B version in patch", args{"v1.0.1", "v1.0.0"}, true},
+		{"A should be less than B version in major", args{"v1.0.0", "v2.0.0"}, false},
+		{"A should be less than B version in minor", args{"v1.0.0", "v1.1.0"}, false},
+		{"A should be less than B version in patch", args{"v1.0.0", "v1.0.1"}, false},
+	}
+
+	for n := 0; n < b.N; n++ {
+		tC := testCases[n%len(testCases)]
+
+		got := CompareVersion(tC.args.(args).av, tC.args.(args).bv)
+
+		if got != tC.want.(bool) {
+			b.Errorf("Benchmark - v.IsGreaterThan() = %v, want %v", got, tC.want.(bool))
+		}
+	}
+}
 
 func TestIsEqualTo(t *testing.T) {
+	type args struct {
+		av string
+		bv string
+	}
+
 	testCases := []testCase{
 		{"It should return true when it was got equal versions but names are different (without strict)", args{"v1.0.0-beta", "v1.0.0"}, true},
 		{"It should return false when it was got equal versions but names are different (with strict)", args{"v1.0.0-beta", "v1.0.0"}, false},
@@ -67,8 +160,8 @@ func TestIsEqualTo(t *testing.T) {
 
 	for _, tC := range testCases {
 		t.Run(tC.name, func(t *testing.T) {
-			aVersion, aErr := NewVersion(tC.args.av)
-			bVersion, bErr := NewVersion(tC.args.bv)
+			aVersion, aErr := NewVersion(tC.args.(args).av)
+			bVersion, bErr := NewVersion(tC.args.(args).bv)
 
 			t.Logf("Versions: %s - %s\n", aVersion.String(), bVersion.String())
 
@@ -83,8 +176,8 @@ func TestIsEqualTo(t *testing.T) {
 			}
 			got := aVersion.IsEqualTo(*bVersion, isStrict)
 
-			if got != tC.want {
-				t.Errorf("v.IsEqualTo() = %v, want %v", got, tC.want)
+			if got != tC.want.(bool) {
+				t.Errorf("v.IsEqualTo() = %v, want %v", got, tC.want.(bool))
 			}
 		})
 	}
