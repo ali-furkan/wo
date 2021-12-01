96a5b29929c574115659c8b78f7a4f23c9d23891
Ali Furkan <46776661+ali-furkan@users.noreply.github.com>
2021-12-01 13:29:13 +0300

    :recycle:: refactor design of config package

---
+++ internal/config/config_map.go
@@ -0,0 +1,153 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	ErrConfigMapFieldNotFound    = "field value not found"
+	ErrConfigMapInvalidFieldAddr = "invalid field addr"
+	ErrConfigValidation          = "%s validation err: %s"
+)
+
+type ConfigMap struct {
+	root     map[string]interface{}
+	defaults map[string]interface{}
+	schema   []FieldSchema
+}
+
+func NewConfigMap(root map[string]interface{}, schema []FieldSchema) *ConfigMap {
+	defaults := genSchemaMap(schema)
+
+	cm := &ConfigMap{
+		root:     root,
+		defaults: defaults,
+		schema:   schema,
+	}
+	return cm
+}
+
+func (cm *ConfigMap) Get(field string) interface{} {
+	ks := strings.Split(field, ".")
+	lastKey := ks[len(ks)-1]
+
+	nestedMap, err := cm.searchMap(&cm.root, ks[:len(ks)-1]...)
+	if err != nil {
+		m, err := cm.searchMap(&cm.defaults, ks[:len(ks)-1]...)
+		if err != nil {
+			return nil
+		}
+
+		return m[lastKey]
+	}
+
+	return nestedMap[lastKey]
+}
+
+func (cm *ConfigMap) GetString(field string) string {
+	val := cm.Get(field)
+	if val == nil || reflect.TypeOf(val).Kind() != reflect.String {
+		return ""
+	}
+
+	return fmt.Sprintf("%v", val)
+}
+
+func (cm *ConfigMap) GetSchema(key string) (*FieldSchema, error) {
+	for _, s := range cm.schema {
+		if s.Key == key {
+			return &s, nil
+		}
+	}
+
+	return nil, errors.New(ErrConfigMapFieldNotFound)
+}
+
+func (cm *ConfigMap) Set(field string, val interface{}) error {
+	ks := strings.Split(field, ".")
+	lastKey := ks[len(ks)-1]
+
+	nestedMap, err := cm.searchMap(&cm.root, ks[:len(ks)-1]...)
+	if err != nil {
+		return err
+	}
+
+	err = cm.validate(val, field)
+	if err != nil {
+		return err
+	}
+
+	nestedMap[lastKey] = val
+	return nil
+}
+
+func (cm *ConfigMap) Reset(field string) error {
+	ks := strings.Split(field, ".")
+	lastKey := ks[len(ks)-1]
+
+	nestedMap, err := cm.searchMap(&cm.root, ks[:len(ks)-1]...)
+	if err != nil {
+		return err
+	}
+
+	return cm.Set(field, nestedMap[lastKey])
+}
+
+func (cm *ConfigMap) Byte() ([]byte, error) {
+	return yaml.Marshal(cm.root)
+}
+
+func (cm ConfigMap) Map() map[string]interface{} {
+	return cm.root
+}
+
+func (cm *ConfigMap) validate(val interface{}, field string) error {
+	ks := strings.Split(field, ".")
+	lastKey := ks[len(ks)-1]
+
+	schema, err := cm.GetSchema(lastKey)
+	if err != nil {
+		return err
+	}
+
+	err = validation.Validate(val, schema.Rules...)
+	if err != nil {
+		return fmt.Errorf(ErrConfigValidation, schema.Key, err)
+	}
+
+	return nil
+}
+
+func (cm *ConfigMap) searchMap(m *map[string]interface{}, keys ...string) (map[string]interface{}, error) {
+	var (
+		v  map[string]interface{} = *m
+		ok bool
+	)
+
+	if len(keys) == 0 {
+		return v, nil
+	}
+
+	for i, k := range keys {
+		v, ok = v[k].(map[string]interface{})
+		if !ok {
+			return nil, errors.New(ErrConfigMapInvalidFieldAddr)
+		}
+
+		if len(keys)-i == 1 {
+			return v, nil
+		}
+
+		if len(keys)-i > 1 && reflect.TypeOf(v).Kind() != reflect.Map {
+			return v, errors.New(ErrConfigMapInvalidFieldAddr)
+		}
+	}
+
+	return nil, errors.New(ErrConfigMapInvalidFieldAddr)
+}
---
+++ internal/config/config_type.go
@@ -0,0 +1,82 @@
+package config
+
+import "path/filepath"
+
+type Config interface {
+	Get(string) interface{}
+	GetString(string) string
+	Set(string, interface{}) error
+	UnSet(string) error
+	Reset()
+	Write() error
+	Map() map[string]interface{}
+}
+
+type fileConfig struct {
+	m      ConfigMap
+	source string
+}
+
+func NewConfig(source string, schema []FieldSchema) (Config, error) {
+	m, err := parseConfigFile(source)
+	if err != nil {
+		return nil, err
+	}
+
+	configMap := NewConfigMap(*m, schema)
+
+	return &fileConfig{
+		source: source,
+		m:      *configMap,
+	}, nil
+}
+
+func NewGlobalConfig() (Config, error) {
+	path := filepath.Join(ConfigDir(), ConfigFileName)
+
+	return NewConfig(path, ConfigGlobalSchema)
+}
+
+func NewBlankConfig() (Config, error) {
+	err := createBlankConfigFile()
+	if err != nil {
+		return nil, err
+	}
+
+	source := filepath.Join(ConfigDir(), ConfigFileName)
+
+	return NewConfig(source, ConfigGlobalSchema)
+}
+
+func (fc *fileConfig) Get(field string) interface{} {
+	return fc.m.Get(field)
+}
+
+func (fc *fileConfig) GetString(field string) string {
+	return fc.m.GetString(field)
+}
+
+func (fc *fileConfig) Set(field string, val interface{}) error {
+	return fc.m.Set(field, val)
+}
+
+func (fc *fileConfig) UnSet(field string) error {
+	return fc.m.Reset(field)
+}
+
+func (fc *fileConfig) Reset() {
+	fc.m.root = fc.m.defaults
+}
+
+func (fc fileConfig) Map() map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func (fc fileConfig) Write() error {
+	data, err := fc.m.Byte()
+	if err != nil {
+		return err
+	}
+
+	return writeConfigFile(fc.source, data)
+}
