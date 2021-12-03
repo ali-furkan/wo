96a43b4efe400ce2d4a608a05adad8db79fb41fe
Ali Furkan <46776661+ali-furkan@users.noreply.github.com>
2021-12-03 19:07:47 +0300

    :sparkles:: add convert funcs for space structs

--- internal/space/types.go
+++ internal/space/types.go
@@ -10,11 +10,70 @@ type Options struct {
 	Editor        string `json:"editor"`
 }
 
+func (o *Options) Map() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	m["readme"] = o.Readme
+	m["code_of_conduct"] = o.CodeOfConduct
+	m["contributing"] = o.Contributing
+	m["license"] = o.License
+	m["git"] = o.Git
+	m["gitignore"] = o.Gitignore
+	m["editor"] = o.Editor
+
+	return m
+}
+
 type Space struct {
-	TempDir  string               `json:"temp_dir"`
-	RootDir  string               `json:"root_dir"`
-	Defaults Options              `json:"defaults"`
-	Works    map[string]Workspace `json:"works"`
+	ID          string               `json:"id"`
+	Description string               `json:"description"`
+	TempDir     string               `json:"temp_dir"`
+	RootDir     string               `json:"root_dir"`
+	Defaults    Options              `json:"defaults"`
+	CreatedAt   int64                `json:"created_at"`
+	Workspaces  map[string]Workspace `json:"workspaces"`
+}
+
+func (s *Space) Map() map[string]interface{} {
+	m := s.MapWithoutWorkspaces()
+
+	m["defaults"] = s.Defaults.Map()
+
+	if s.Workspaces != nil || len(s.Workspaces) > 0 {
+		mws := make(map[string]interface{})
+		for id, ws := range s.Workspaces {
+			mws[id] = ws.Map()
+		}
+
+		m["workspaces"] = mws
+	}
+
+	return m
+}
+
+func (s *Space) MapForConfig() map[string]interface{} {
+	m := s.MapWithoutWorkspaces()
+
+	mws := make(map[string]interface{})
+	for id, ws := range s.Workspaces {
+		mws[id] = ws.MapForConfig()
+	}
+
+	m["workspaces"] = mws
+
+	return m
+}
+
+func (s *Space) MapWithoutWorkspaces() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	m["id"] = s.ID
+	m["root_dir"] = s.RootDir
+	m["temp_dir"] = s.TempDir
+	m["description"] = s.Description
+	m["created_at"] = s.CreatedAt
+
+	return m
 }
 
 type Workspace struct {
@@ -28,6 +87,76 @@ type Workspace struct {
 	CreatedAt   int64    `json:"created_at"`
 }
 
+func (ws *Workspace) Map() map[string]interface{} {
+	m := ws.MapForRC()
+
+	m["id"] = ws.ID
+	m["path"] = ws.Path
+	m["created_at"] = ws.CreatedAt
+
+	return m
+}
+
+func (ws *Workspace) MapForRC() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	m["name"] = ws.Name
+	m["description"] = ws.Description
+	m["run"] = ws.Run
+
+	if ws.Flows != nil {
+		flows := []map[string]interface{}{}
+		for i, f := range ws.Flows {
+			flows[i] = f.Map()
+		}
+	}
+
+	if ws.Actions != nil {
+		actions := []map[string]interface{}{}
+		for i, a := range ws.Actions {
+			actions[i] = a.Map()
+		}
+	}
+
+	return m
+}
+
+func (ws *Workspace) MapForConfig() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	m["id"] = ws.ID
+	m["name"] = ws.Name
+	m["path"] = ws.Path
+	m["created_at"] = ws.CreatedAt
+
+	return m
+}
+
+type Flows struct {
+	Name       string   `json:"name"`
+	Env        string   `json:"env"`
+	WorkingDir string   `json:"working_dir"`
+	Steps      []Action `json:"steps"`
+}
+
+func (f *Flows) Map() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	m["name"] = f.Name
+	m["env"] = f.Env
+	m["working_dir"] = f.WorkingDir
+
+	steps := []map[string]interface{}{}
+
+	for i, s := range f.Steps {
+		steps[i] = s.Map()
+	}
+
+	m["steps"] = steps
+
+	return m
+}
+
 type Action struct {
 	Name       string   `json:"name"`
 	Env        []string `json:"env"`
@@ -36,9 +165,14 @@ type Action struct {
 	Workingdir string   `json:"working_dir"`
 }
 
-type Flows struct {
-	Name       string   `json:"name"`
-	Env        string   `json:"env"`
-	WorkingDir string   `json:"working_dir"`
-	Steps      []Action `json:"steps"`
+func (a *Action) Map() map[string]interface{} {
+	m := make(map[string]interface{})
+
+	m["name"] = a.Name
+	m["env"] = a.Env
+	m["args"] = a.Args
+	m["run"] = a.Run
+	m["working_dir"] = a.Workingdir
+
+	return m
 }
