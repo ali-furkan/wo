04dd383482a8b7a5b1293fa11a959320f28547ff
Ali Furkan <46776661+ali-furkan@users.noreply.github.com>
2021-12-03 19:12:26 +0300

    :art:: added validate type of editor struct

--- internal/editor/types.go
+++ internal/editor/types.go
@@ -1,6 +1,26 @@
 package editor
 
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
 type Editor struct {
-	Name string `yaml:"name"`
-	Exec string `yaml:"execute_path"`
+	Name string `json:"name"`
+	ID   string `json:"id"`
+	Exec string `json:"exec"`
+}
+
+var (
+	EditorNameRule = []validation.Rule{validation.Required, validation.Length(3, 32), is.PrintableASCII}
+	EditorIDRule   = []validation.Rule{validation.Required, validation.Length(3, 32), is.Alphanumeric}
+	EditorExecRule = []validation.Rule{validation.Required}
+)
+
+func (e *Editor) Validate() error {
+	return validation.ValidateStruct(e,
+		validation.Field(&e.Name, EditorNameRule...),
+		validation.Field(&e.ID, EditorIDRule...),
+		validation.Field(&e.Exec, EditorExecRule...),
+	)
 }
